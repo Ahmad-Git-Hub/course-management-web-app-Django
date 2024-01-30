12135d74bbeadee523730527b28f915054c24889
Ahmad M.Naji <ahmad.mohamad.cs@gmail.com>
2024-01-30 23:53:21 +0300

    Modify admin.py create LecturerAdmin class, set default sorting in CourseAdmin class and adding new function days_since_creation

--- core/admin.py
+++ core/admin.py
@@ -1,30 +1,51 @@
+import datetime
 from django.contrib import admin
 from .models import Lecturer, Course
 
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ['lecturer_name', 'qualification', 'title_rank', 'gender', 'cv_link']
+    ordering = ['title_rank']
+
+
 
 class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ['course_name', 'starting_date', 'price', 'creation_date', 'lecturer', 'days_since_creation']
+
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if not request.user.is_superuser:
             # Filter courses based on the lecturer for non-superusers
             qs = qs.filter(lecturer=request.user.lecturer)
-        return qs
+        return qs.order_by('starting_date')
   
 
     def get_form(self, request, obj=None, **kwargs):
     # Customize the form based on the user's permissions
         if not request.user.is_superuser:
-            self.exclude = ('lecturer',)
+            self.exclude = ('lecturer', 'creation_date')
         else:
             self.exclude = ()
         return super().get_form(request, obj, **kwargs)
         
 
     def save_model(self, request, course, form, change):
-        course.lecturer = request.user.lecturer
+        if request.user.is_superuser:
+            course.lecturer = form.cleaned_data['lecturer']
+        else:
+            course.lecturer = request.user.lecturer
         super().save_model(request, course, form, change)
 
+   
+    def days_since_creation(self, course):
+        days = (datetime.date.today() - course.creation_date).days
+        return days
+    
+    days_since_creation.short_description = "Days Since Creation"
+  
+
+
         
-admin.site.register(Lecturer)
+admin.site.register(Lecturer, LecturerAdmin)
 admin.site.register(Course, CourseAdmin)
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Lecturer, Course
 
 
 def home(request):
@@ -6,11 +7,20 @@ def home(request):
 
 
 def course(request):
-    
-    return render(request, 'core/course.html',)
+    courses = Course.objects.all()
+    context = {'courses': courses}
+    return render(request, 'core/course.html', context)
 
 
 def lecturer(request):
+    lecturers = Lecturer.objects.all()
+    lecturer_name = request.GET.get('lecturer_name', '').lower()
+    sort_by = request.GET.get('sort_by', '')
+    if lecturer_name:
+        lecturers = lecturers.filter(lecturer_name__icontains=lecturer_name)
+    if sort_by:
+        lecturers = lecturers.order_by(f"-{sort_by}" if sort_by == "qualification" else sort_by)
+    context = {'lecturers': lecturers}
+    return render(request, 'core/lecturer.html', context)
     
     
-    return render(request, 'core/lecturer.html',)
