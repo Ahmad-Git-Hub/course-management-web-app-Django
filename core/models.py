5de71040cf912473fa5c24b027192a2fba6118f8
Ahmad M.Naji <ahmad.mohamad.cs@gmail.com>
2024-01-30 23:56:25 +0300

    Adding creation_date field, adding unique constraint in course model

--- core/models.py
+++ core/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 
 class Lecturer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    lecturer_name = models.CharField(max_length=50, null=False)
+    lecturer_name = models.CharField(max_length=50, null=False, unique = True)
     qualification = models.CharField(max_length=50, null=True, blank=True)
     TITLE_RANK_CHOICES = ((1, "Professor"), (2, "Assistant Professor"),
                           (3, 'Lecturer'), (4, 'Assistant Lecturer'),)
     title_rank = models.SmallIntegerField(
         choices=TITLE_RANK_CHOICES, default=4)
     department = models.CharField(max_length=50, null=True, blank=True)
-    cv_link = models.URLField(max_length=255, null=True, blank=True)
+    cv_link = models.URLField(max_length=255, null=True, blank=True, unique=True)
     gender = models.CharField(max_length=1, null=True, blank=True, choices=[
                               ('M', 'Male'), ('F', 'Female')])
     
@@ -21,9 +21,10 @@ class Lecturer(models.Model):
 
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
-    course_name = models.CharField(max_length=155)
+    course_name = models.CharField(max_length=155, unique=True)
     description = models.TextField(blank=True)
     prerequisites = models.TextField(blank=True)
+    creation_date = models.DateField(auto_now_add = True)
     starting_date = models.DateField(blank=True, null=True)
     duration_days = models.IntegerField(blank=True, null=True)
     price = models.DecimalField(
