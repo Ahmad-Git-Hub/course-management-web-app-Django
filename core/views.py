4d8d0e2c653969d6b6883c5e2f4c3f09e8d0790a
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-03-30 20:59:54 +0300

    Implement search functionality in course template and edit course view accordingly

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render
 from .models import Lecturer, Course
 
@@ -7,10 +8,13 @@ def home(request):
 
 
 def course(request):
-    courses = Course.objects.all()
+    lecturer_name = request.GET.get('lecturer_name', None) 
+    today = date.today() 
+    courses = Course.objects.filter(starting_date__gt=today)  
+    if lecturer_name:
+        courses = courses.filter(lecturer__lecturer_name__icontains=lecturer_name) 
     context = {'courses': courses}
     return render(request, 'core/course.html', context)
-
 
 def lecturer(request):
     lecturers = Lecturer.objects.all()
