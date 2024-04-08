819b03501f530ecc1d69652afa11580535999798
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-04-08 17:01:45 +0300

    Create new template and css file course_detail and make the necessary changes

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='core-home'),
     path('course/', views.course, name='core-course'),
+    path('course/<int:course_id>/', views.course_detail, name='core-course-detail'),  # new line
     path('lecturer/', views.lecturer, name='core-lecturer'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Lecturer, Course
 
 
@@ -7,13 +7,18 @@ def home(request):
     return render(request, 'core/index.html')
 
 
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, course_id=course_id)
+    context = {'course': course, 'current_page': 'course_detail'}
+    return render(request, 'core/course_detail.html', context)
+
 def course(request):
     course_name = request.GET.get('course_name', None) 
     today = date.today() 
     courses = Course.objects.filter(starting_date__gt=today)  
     if course_name:
         courses = courses.filter(course_name__icontains=course_name) 
-    context = {'courses': courses}
+    context = {'courses': courses, 'current_page': 'course'}
     return render(request, 'core/course.html', context)
 
 def lecturer(request):
@@ -24,7 +29,7 @@ def lecturer(request):
         lecturers = lecturers.filter(lecturer_name__icontains=lecturer_name)
     if sort_by:
         lecturers = lecturers.order_by(f"-{sort_by}" if sort_by == "qualification" else sort_by)
-    context = {'lecturers': lecturers}
+    context = {'lecturers': lecturers, 'current_page': 'lecturer'}
     return render(request, 'core/lecturer.html', context)
     
     
